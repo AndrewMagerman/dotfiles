0be62924fe87735526fdfb12e0ad44c1a5db3375
Andrew Magerman <andrew.magerman@magerman.com>
2025-08-29 20:55:51 +0200

    c specific config

--- dot_config/nvim/ftplugin/c.lua
+++ dot_config/nvim/ftplugin/c.lua
@@ -1,3 +1,15 @@
+--- avoid re-loading if filetype plugin is sourced twice
+if vim.b.c_ftplugin_loaded then return end
+vim.b.c_ftplugin_loaded = true
+
+-- Insert mode: typing ";;" appends a semicolon at end of the line,
+-- executes as a one-shot normal command, then stays in insert mode.
+vim.keymap.set("i", ";;", "<C-o>A;", {
+  buffer = true,
+  silent = true,
+  desc = "Insert ; at end of line",
+})
+
 -- Abbreviation for '->' in C files
 vim.cmd("iabbrev ,, ->")
 
