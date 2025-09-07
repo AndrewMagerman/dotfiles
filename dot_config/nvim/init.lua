e66885a7d1afbd25840a4e788845432d6047be76
Andrew Magerman <andrew.magerman@magerman.com>
2025-09-07 15:57:19 +0200

    removed redundant harpoon configuration

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -910,15 +910,8 @@ require('lazy').setup({
     --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
     --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
   },
-  {
-    'ThePrimeagen/harpoon',
-    branch = 'harpoon2',
-    dependencies = { 'nvim-lua/plenary.nvim' },
-  },
-
-  -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
-  -- init.lua. If you want these files, they are in the repository, so you can just download them and
-  -- place them in the correct locations.
+  -- NOTE: This sets up your configuration file manager and other UI elements.
+  -- Take a look at the kickstart.nvim README for more information
 
   -- NOTE: Next step on your Neovim journey: Add/Configure additional plugins for Kickstart
   --
