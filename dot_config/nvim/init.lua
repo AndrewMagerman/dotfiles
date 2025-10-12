8f1228395b0849839960b8dc53f5d6e66e1c7530
Andrew Magerman <andrew.magerman@magerman.com>
2025-10-12 21:29:17 +0200

    added clangd LSP linking

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -4,4 +4,4 @@ require("config.lazy")
 
 vim.lsp.enable("basedpyright")   
 vim.lsp.enable("lua_ls")   
-
+vim.lsp.enable("clangd")
---
+++ dot_config/nvim/lsp/clangd.lua
@@ -0,0 +1,20 @@
+-- Minimal clangd config for Neovim 0.11 builtin LSP.
+-- Enable with: vim.lsp.enable("clangd")
+
+local M = {}
+
+M.cmd = { "clangd" }  -- use full path if not on $PATH
+
+-- Helps avoid offsetEncoding warnings with some tools
+M.capabilities = (function()
+  local c = vim.lsp.protocol.make_client_capabilities()
+  c.offsetEncoding = { "utf-16" }
+  return c
+end)()
+
+-- Keep it simple: detect project root
+M.root_markers = { "compile_commands.json", ".clangd", ".git" }
+
+
+return M
+
