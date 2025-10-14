f54f85846cd277620ae293d70a2412640d52dbd3
Andrew Magerman <andrew.magerman@magerman.com>
2025-10-14 21:49:06 +0200

    add options for ignoring swapfiles

--- dot_config/nvim/lua/options.lua
+++ dot_config/nvim/lua/options.lua
@@ -1,2 +1,6 @@
 vim.opt.nu = true -- enable line numbers
 vim.opt.relativenumber = true -- relative line numbers
+
+vim.opt.swapfile = false
+vim.opt.undofile = true
+vim.opt.undodir = vim.fn.stdpath("state") .. "/undo//"
