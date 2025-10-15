6885593b5c58604492577d7f88d685849a23c650
Andrew Magerman <andrew.magerman@magerman.com>
2025-10-15 12:02:34 +0200

    added some new options

--- dot_config/nvim/lua/options.lua
+++ dot_config/nvim/lua/options.lua
@@ -1,6 +1,32 @@
 vim.opt.nu = true -- enable line numbers
 vim.opt.relativenumber = true -- relative line numbers
 
+vim.opt.spelllang = { "en", "de" } -- Set language for spellchecking
+
 vim.opt.swapfile = false
 vim.opt.undofile = true
 vim.opt.undodir = vim.fn.stdpath("state") .. "/undo//"
+
+
+vim.o.mouse = 'a'
+vim.opt.clipboard:append("unnamedplus") -- Use system clipboard
+vim.opt.encoding = "UTF-8" -- Use UTF-8 encoding
+
+-- Minimal number of screen lines to keep above and below the cursor.
+vim.o.scrolloff = 10
+
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+vim.opt.smarttab = true
+vim.opt.smartindent = true -- Smart auto-indenting
+vim.opt.autoindent = true -- Copy indent from current line
+vim.opt.grepprg = "rg --vimgrep" -- Use ripgrep if available
+vim.opt.grepformat = "%f:%l:%c:%m" -- filename, line number, column, content
+
+vim.opt.termguicolors = true -- Enable 24-bit colors
+vim.opt.signcolumn = "yes" -- Always show sign column
+vim.opt.colorcolumn = "100" -- Show column at 100 characters
+vim.opt.showmatch = true -- Highlight matching brackets
+vim.opt.matchtime = 2 -- How long to show matching bracket
