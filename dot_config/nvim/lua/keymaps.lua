c386997ed7d84c9d33b260e365da0354aa1b97f7
Andrew Magerman <andrew.magerman@magerman.com>
2025-10-20 18:19:55 +0200

    add marksman as an LSP and the keybinding for formatting

--- dot_config/nvim/lua/keymaps.lua
+++ dot_config/nvim/lua/keymaps.lua
@@ -1,2 +1,7 @@
 -- space bar leader key
 vim.g.mapleader = ' '
+
+-- formatting
+vim.keymap.set("n", "<leader>f", function()
+  vim.lsp.buf.format({ async = false })
+end, { desc = "Format file with LSP" })
