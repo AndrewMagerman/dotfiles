58c2d5b1f029f44af793e76c143264ca19ad8bc1
Andrew Magerman <andrew.magerman@postfinance.ch>
2025-10-27 15:09:21 +0100

    neotree: show dotfiles

--- dot_config/nvim/lua/plugins/neo-tree.lua
+++ dot_config/nvim/lua/plugins/neo-tree.lua
@@ -15,6 +15,12 @@ return {
   },
   opts = {
     filesystem = {
+        filtered_items = {
+            visible = true,
+            hide_dotfiles = false,
+            hide_gitignored = false,
+            hide_by_name = { ".idea" },
+                         },
       window = {
         mappings = {
           ['\\'] = 'close_window',
