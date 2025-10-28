bdac07045ceaa983ccec6816f5f233e255823d73
Andrew Magerman <andrew.magerman@postfinance.ch>
2025-10-28 11:04:01 +0100

    correct typo

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -3,7 +3,16 @@ require("options")
 require("config.lazy")
 require("automaps")
 
-vim.lsp.enable("basedpyright")   
-vim.lsp.enable("lua_ls")   
+vim.lsp.enable("basedpyright")
+vim.lsp.enable("lua_ls")
 vim.lsp.enable("clangd")
 vim.lsp.enable("marksman")
+vim.lsp.enable("ruff")
+
+vim.diagnostic.config(
+    {
+        virtual_text = { source = true },
+        float = { source = "always" },
+    }
+
+)
