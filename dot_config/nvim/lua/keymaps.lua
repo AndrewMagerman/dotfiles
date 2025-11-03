b22473a6ef9570d4622bdc9fc395cd02d36edb74
Andrew Magerman <andrew.magerman@postfinance.ch>
2025-11-03 11:39:49 +0100

    remove wrong keymap

--- dot_config/nvim/lua/keymaps.lua
+++ dot_config/nvim/lua/keymaps.lua
@@ -1,7 +1,2 @@
 -- space bar leader key
 vim.g.mapleader = ' '
-
--- formatting
-vim.keymap.set("n", "<leader>f", function()
-  vim.lsp.buf.format({ async = false })
-end, { desc = "Format file with LSP" })
