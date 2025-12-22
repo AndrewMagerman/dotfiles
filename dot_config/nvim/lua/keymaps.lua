c6dd1534643ea6a8cafc750cc76a05bacae99915
Andrew Magerman <andrew.magerman@magerman.com>
2025-12-22 15:21:27 +0100

    adds format command

--- dot_config/nvim/lua/keymaps.lua
+++ dot_config/nvim/lua/keymaps.lua
@@ -1,2 +1,8 @@
 -- space bar leader key
 vim.g.mapleader = ' '
+
+vim.keymap.set({ 'n', 'v' },
+    '<leader>f', function()
+        vim.lsp.buf.format { async = true }
+    end,
+    { desc = '[F]ormat buffer' })
