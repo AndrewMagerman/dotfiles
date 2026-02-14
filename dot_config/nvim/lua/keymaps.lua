df440237bbd2d3f0aeb3139c76a2e5075042de67
Andrew Magerman <andrew.magerman@postfinance.ch>
2026-02-14 13:29:03 +0100

    add copy path to clipboard

--- dot_config/nvim/lua/keymaps.lua
+++ dot_config/nvim/lua/keymaps.lua
@@ -1,10 +1,14 @@
 -- space bar leader key
 vim.g.mapleader = ' '
 
-vim.keymap.set({ 'n', 'v' },
-    '<leader>f', function()
-        vim.lsp.buf.format { async = true }
-    end,
-    { desc = '[F]ormat buffer' })
+-- vim.keymap.set({ 'n', 'v' },
+    -- '<leader>f', function()
+        -- vim.lsp.buf.format { async = true }
+    -- end,
+    -- { desc = '[F]ormat buffer' })
 
 vim.keymap.set("n", "gl", vim.diagnostic.open_float, { desc = "Line diagnostics" })
+
+vim.keymap.set("n", "<leader>cp", function()
+  vim.fn.setreg("+", vim.fn.expand("%:p"))
+end, { desc = "Copy file path to clipboard" })
