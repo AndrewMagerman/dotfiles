d22c3228f546ac67fa884745a0aec55b6395dbf3
Andrew Magerman <andrew.magerman@magerman.com>
2026-03-08 23:02:30 +0100

    correct formatting issues

--- dot_config/nvim/lua/plugins/conform.lua
+++ dot_config/nvim/lua/plugins/conform.lua
@@ -1,14 +1,37 @@
 return {
     "stevearc/conform.nvim",
     event = { "BufReadPre", "BufNewFile" },
-    opts = {
-        notify_on_error = false,
-        -- format_on_save = { timeout_ms = 1000, lsp_fallback = true },
-        formatters_by_ft = {
-            python = { "ruff_fix", "ruff_format" },
-        },
-    },
+    opts = function()
+        local clang_format = vim.fn.exepath("clang-format")
+
+        return {
+            notify_on_error = true,
+            -- format_on_save = { timeout_ms = 1000, lsp_fallback = true },
+            formatters = {
+                clang_format = {
+                    command = clang_format ~= "" and clang_format or "clang-format",
+                },
+            },
+            formatters_by_ft = {
+                python = { "ruff_fix", "ruff_format" },
+                c = { "clang_format" },
+                cpp = { "clang_format" },
+                objc = { "clang_format" },
+                objcpp = { "clang_format" },
+            },
+        }
+    end,
     keys = {
-        { "<leader>f", function() require("conform").format() end, desc = "Format file (Ruff for python), LSP else" },
+        {
+            "<leader>f",
+            function()
+                require("conform").format({
+                    async = false,
+                    timeout_ms = 2000,
+                    lsp_format = "fallback",
+                })
+            end,
+            desc = "Format file (Ruff for python, clang-format for C-family)",
+        },
     },
 }
